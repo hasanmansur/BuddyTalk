4ae863aa9d40e814d7e03686f4aa463b804bc32e
smhasanmansur <smhasanmansur@gmail.com>
2016-06-15 01:04:39 +0600

    room create & join | multicasting

--- index.js
+++ index.js
@@ -10,27 +10,44 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+var users = [];
+var rooms = [];
+
 io.on('connection', function (socket) {
 	//event listeners
 	socket.on('join', function (msg) {
 		socket.username = msg.email;
-		console.log(socket.username + ' joined');
-		socket.broadcast.emit('join', socket.username);
+		users.push(socket.username);
+		socket.emit('login', { username: socket.username, rooms: rooms });
 		socket.join(socket.username);
+		io.emit('join', { username: socket.username, users: users });
 	});
 	socket.on('disconnect', function () {
-		console.log(socket.username + ' left');
-		socket.broadcast.emit('leave', socket.username);
+		var i = users.indexOf(socket.username);
+		if (i > -1) {
+			users.splice(i,1);
+		}
+		io.emit('leave', { username: socket.username, users: users });
 	});
-	socket.on('chat message', function (msg, user) {
-		console.log(socket.username + ': ' + msg);
+	socket.on('chat message', function (msg, user, room) {
 		if (user) {
 			socket.broadcast.to(user).emit('chat message', { username: socket.username, message: msg });
 		}
-		else {
+		if (room) {
+			socket.broadcast.to(room).emit('chat message', { username: socket.username, message: msg });
+		}
+		if (!user && !room) {
 			socket.broadcast.emit('chat message', { username: socket.username, message: msg });	
 		}
 		
+	});
+	socket.on('createRoom', function (msg) {
+		rooms.push(msg.room);
+		socket.join(msg.room);
+		io.emit('createRoom', { rooms: rooms });
+	});
+	socket.on('joinRoom', function (msg) {
+		socket.join(msg.room);
 	});
 });
 
